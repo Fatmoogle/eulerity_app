018a4f8e36e8dcedb6d2bc6662f328ecb6bd59e8
Alex Varela <alexvar93@gmail.com>
2021-09-09 20:55:49 -0400

    added nav component, not working

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
 
 export default function Nav() {
     return (
-        <div>
-            <h1>navbar</h1>
-        </div>
+        <Navbar>
+            <Logo href="">
+                <span>PetsLuv</span>
+            </Logo>
+            <Hamburger>
+                <span />
+                <span />
+                <span />
+            </Hamburger>
+            <Menu>
+                <MenuLink href="">About Me</MenuLink>
+                <MenuLink href="">Gallery</MenuLink>
+            </Menu> 
+        </Navbar>
     )
 }
+
+const Navbar = styled.div`
+    padding: 0 2rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+    background: white;
+`;
+
+const Hamburger = styled.div`
+
+`;
+
+const MenuLink = styled.a`
+
+`;
+
+const Menu = styled.div`
+
+`;
+
+const Logo = styled.a`
+
+`;
